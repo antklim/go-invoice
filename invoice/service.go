2a4a4442e6f53849677a44550863eb6a2abf3d14
antklim <antklim@gmail.com>
2021-08-11 08:48:38 +1000

    Reset project

--- invoice/service.go
+++ invoice/service.go
@@ -5,35 +5,15 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Storage interface {
-	AddInvoice(Invoice) error
-	GetInvoice(string) (Invoice, error)
-}
-
-type Service struct {
-	storage Storage
-}
-
-func NewService(storage Storage) Service {
-	return Service{storage: storage}
-}
+type Service struct{}
 
 func (s Service) CreateInvoice(customerName string) (Invoice, error) {
 	invID := uuid.NewString()
-	inv := createInvoice(invID, customerName)
-	err := s.storage.AddInvoice(inv)
-	if err != nil {
-		return Invoice{}, errors.Wrap(err, "failed to store invoice")
-	}
-	return inv, nil
+	return createInvoice(invID, customerName), nil
 }
 
 func (s Service) ViewInvoice(id string) (Invoice, error) {
-	inv, err := s.storage.GetInvoice(id)
-	if err != nil {
-		return Invoice{}, errors.Wrap(err, "failed to get invoice")
-	}
-	return inv, nil
+	return Invoice{}, errors.New("not implemented")
 }
 
 func (s Service) UpdateInvoiceCustomer(id, name string) error {
--- invoice/service_test.go
+++ invoice/service_test.go
@@ -1,39 +1,18 @@
 package invoice_test
 
 import (
-	"errors"
 	"testing"
 	"time"
 
 	"github.com/antklim/go-invoice/invoice"
 )
 
-type testStorage struct {
-	e   error
-	inv invoice.Invoice
-}
-
-func (storage *testStorage) AddInvoice(inv invoice.Invoice) error {
-	if storage.e != nil {
-		return storage.e
-	}
-
-	storage.inv = inv
-	return nil
-}
-
-func (storage testStorage) GetInvoice(id string) (invoice.Invoice, error) {
-	return storage.inv, storage.e
-}
-
-var testErrStorage = &testStorage{e: errors.New("storage error")}
-
 func TestCreateInvoice(t *testing.T) {
 	t.Run("creates valid invoice", func(t *testing.T) {
-		srv := invoice.NewService(&testStorage{})
+		srv := invoice.Service{}
 		inv, err := srv.CreateInvoice("John Doe")
 		if err != nil {
-			t.Fatalf("Error creating invoice: %v", err)
+			t.Fatalf("error creating invoice: %v", err)
 		}
 
 		if inv.ID == "" {
@@ -59,237 +38,93 @@ func TestCreateInvoice(t *testing.T) {
 		}
 	})
 
-	t.Run("stores invoice in data storage", func(t *testing.T) {
-		srv := invoice.NewService(&testStorage{})
-		inv, err := srv.CreateInvoice("John Doe")
-		if err != nil {
-			t.Fatalf("Error creating invoice: %v", err)
-		}
+	t.Run("stores invoice in data storage", func(t *testing.T) {})
 
-		storedInvoice, err := srv.ViewInvoice(inv.ID)
-		if err != nil {
-			t.Fatalf("Error viewing invoice: %v", err)
-		}
-
-		if storedInvoice != inv {
-			t.Fatalf("stored invoice is %v, want %v", storedInvoice, inv)
-		}
-	})
-
-	t.Run("propagates data storage errors", func(t *testing.T) {
-		srv := invoice.NewService(testErrStorage)
-		inv, err := srv.CreateInvoice("Doe John")
-		expectedErr := "failed to store invoice: storage error"
-		if err.Error() != expectedErr {
-			t.Fatalf("err is %v, want = %s", err, expectedErr)
-		}
-
-		expInv := invoice.Invoice{}
-		if inv != expInv {
-			t.Fatalf("invoice is %v, want = %v", inv, expInv)
-		}
-	})
+	t.Run("propagates data storage errors", func(t *testing.T) {})
 }
 
 func TestViewInvoice(t *testing.T) {
-	t.Run("returns a zero invoice when no invoice is found in data storage", func(t *testing.T) {
-		srv := invoice.NewService(&testStorage{})
-		inv, err := srv.ViewInvoice("37f86bef-041d-4e50-aaf7-b1a066123751")
-		if err != nil {
-			t.Fatalf("Error viewing invoice: %v", err)
-		}
+	t.Run("returns a zero invoice when no invoice is found in data storage", func(t *testing.T) {})
 
-		expInv := invoice.Invoice{}
-		if inv != expInv {
-			t.Fatalf("invoice is %v, want = %v", inv, expInv)
-		}
-	})
-
-	t.Run("propagates data storage errors", func(t *testing.T) {
-		srv := invoice.NewService(testErrStorage)
-		inv, err := srv.ViewInvoice("37f86bef-041d-4e50-aaf7-b1a066123751")
-		expectedErr := "failed to get invoice: storage error"
-		if err.Error() != expectedErr {
-			t.Fatalf("err is %v, want = %s", err, expectedErr)
-		}
-
-		expInv := invoice.Invoice{}
-		if inv != expInv {
-			t.Fatalf("invoice is %v, want = %v", inv, expInv)
-		}
-	})
+	t.Run("propagates data storage errors", func(t *testing.T) {})
 }
 
 func TestUpdateInvoice(t *testing.T) {
-	t.Run("propagates data storage errors", func(t *testing.T) {
-		srv := invoice.NewService(testErrStorage)
-		err := srv.UpdateInvoiceCustomer("37f86bef-041d-4e50-aaf7-b1a066123751", "Foo Bar")
-		expectedErr := "failed to update invoice: storage error"
-		if err.Error() != expectedErr {
-			t.Fatalf("err is %v, want = %s", err, expectedErr)
-		}
-	})
+	t.Run("propagates data storage errors", func(t *testing.T) {})
 }
 
 func TestCloseInvoice(t *testing.T) {
-	t.Run("propagates data storage errors when cancelling invoice", func(t *testing.T) {
-		srv := invoice.NewService(testErrStorage)
-		err := srv.CancelInvoice("37f86bef-041d-4e50-aaf7-b1a066123751")
-		expectedErr := "failed to cancel invoice: storage error"
-		if err.Error() != expectedErr {
-			t.Fatalf("err is %v, want = %s", err, expectedErr)
-		}
-	})
+	t.Run("propagates data storage errors when cancelling invoice", func(t *testing.T) {})
 
-	t.Run("propagates data storage errors when paying invoice", func(t *testing.T) {
-		srv := invoice.NewService(testErrStorage)
-		err := srv.PayInvoice("37f86bef-041d-4e50-aaf7-b1a066123751")
-		expectedErr := "failed to pay invoice: storage error"
-		if err.Error() != expectedErr {
-			t.Fatalf("err is %v, want = %s", err, expectedErr)
-		}
-	})
+	t.Run("propagates data storage errors when paying invoice", func(t *testing.T) {})
 }
 
 // Following are the business rules tests
 func TestOpenInvoice(t *testing.T) {
-	t.Run("can be viewed", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be viewed", func(t *testing.T) {})
 
 	t.Run("can be updated", func(t *testing.T) {
-		t.Run("customer name can be updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("customer name can be updated", func(t *testing.T) {})
 
-		t.Run("items can be added", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items can be added", func(t *testing.T) {})
 
 		t.Run("items can be deleted", func(t *testing.T) {
 			// when deleting non existent item it does not return error
 			// error returned only in case of data access layer
-			t.Log("not implemented")
-			t.Fail()
 		})
 	})
 
 	t.Run("cannot be updated", func(t *testing.T) {
-		t.Run("issue date cannot be updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("issue date cannot be updated", func(t *testing.T) {})
 
-		t.Run("items cannot be updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items cannot be updated", func(t *testing.T) {})
 	})
 
 	t.Run("can be issued", func(t *testing.T) {
-		t.Log("not implemented")
 		// verify issue date is set
-		t.Fail()
 	})
 
-	t.Run("can be cancelled", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be cancelled", func(t *testing.T) {})
 
-	t.Run("cannot be paid", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("cannot be paid", func(t *testing.T) {})
 }
 
 func TestIssuedInvoice(t *testing.T) {
-	t.Run("can be viewed", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be viewed", func(t *testing.T) {})
 
 	t.Run("cannot be updated", func(t *testing.T) {
-		t.Run("customer name cannot updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("customer name cannot updated", func(t *testing.T) {})
 
-		t.Run("issue date cannot be updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("issue date cannot be updated", func(t *testing.T) {})
 
-		t.Run("items cannot be added", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items cannot be added", func(t *testing.T) {})
 
-		t.Run("items cannot be deleted", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items cannot be deleted", func(t *testing.T) {})
 	})
 
-	t.Run("cannot be issued", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("cannot be issued", func(t *testing.T) {})
 
-	t.Run("can be cancelled", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be cancelled", func(t *testing.T) {})
 
-	t.Run("can be paid", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be paid", func(t *testing.T) {})
 }
 
 func TestClosedInvoice(t *testing.T) {
-	t.Run("can be viewed", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("can be viewed", func(t *testing.T) {})
 
 	t.Run("cannot be updated", func(t *testing.T) {
-		t.Run("customer name cannot updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("customer name cannot updated", func(t *testing.T) {})
 
-		t.Run("issue date cannot be updated", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("issue date cannot be updated", func(t *testing.T) {})
 
-		t.Run("items cannot be added", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items cannot be added", func(t *testing.T) {})
 
-		t.Run("items cannot be deleted", func(t *testing.T) {
-			t.Log("not implemented")
-			t.Fail()
-		})
+		t.Run("items cannot be deleted", func(t *testing.T) {})
 	})
 
-	t.Run("cannot be issued", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("cannot be issued", func(t *testing.T) {})
 
-	t.Run("cannot be cancelled", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("cannot be cancelled", func(t *testing.T) {})
 
-	t.Run("cannot be paid", func(t *testing.T) {
-		t.Log("not implemented")
-		t.Fail()
-	})
+	t.Run("cannot be paid", func(t *testing.T) {})
 }
