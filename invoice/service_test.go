a7b5412d88bc79f0bfd0cc94f6dc923d076bdc26
antklim <antklim@gmail.com>
2021-08-29 11:35:35 +1000

    Update customer new test

--- invoice/service_test.go
+++ invoice/service_test.go
@@ -1,6 +1,7 @@
 package invoice_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -80,9 +81,30 @@ func TestViewInvoice(t *testing.T) {
 	t.Run("propagates data storage errors", func(t *testing.T) {})
 }
 
-func TestUpdateInvoiceCustomerFails(t *testing.T) {
-	t.Run("when no invoice found", func(t *testing.T) {})
-	t.Run("when data storage error occurred", func(t *testing.T) {})
+func TestUpdateInvoiceCustomer(t *testing.T) {
+	strg, _ := storage.Factory("memory")
+	srv := invoice.New(strg)
+
+	t.Run("fails when no invoice found", func(t *testing.T) {
+		invID := uuid.Nil.String()
+		customer := "John Doe"
+		err := srv.UpdateInvoiceCustomer(invID, customer)
+		if err == nil {
+			t.Fatalf("expected UpdateInvoiceCustomer(%q, %q) to fail when invoice does not exist", invID, customer)
+		}
+		if got, want := err.Error(), fmt.Sprintf("invoice %q not found", invID); got != want {
+			t.Errorf("UpdateInvoiceCustomer(%q, %q) failed with: %s, want %s", invID, customer, got, want)
+		}
+	})
+
+	t.Run("fails when invoice is in the status other than open", func(t *testing.T) {})
+
+	t.Run("fails when data storage error occurred", func(t *testing.T) {
+		// search failed
+		// update failed
+	})
+
+	t.Run("successfully updates customer name of open invoice", func(t *testing.T) {})
 }
 
 func TestAddInvoiceItemFails(t *testing.T) {
@@ -147,8 +169,6 @@ func TestIssuedInvoice(t *testing.T) {
 	t.Run("can be viewed", func(t *testing.T) {})
 
 	t.Run("cannot be updated", func(t *testing.T) {
-		t.Run("customer name cannot updated", func(t *testing.T) {})
-
 		t.Run("issue date cannot be updated", func(t *testing.T) {})
 
 		t.Run("items cannot be added", func(t *testing.T) {})
@@ -167,8 +187,6 @@ func TestClosedInvoice(t *testing.T) {
 	t.Run("can be viewed", func(t *testing.T) {})
 
 	t.Run("cannot be updated", func(t *testing.T) {
-		t.Run("customer name cannot updated", func(t *testing.T) {})
-
 		t.Run("issue date cannot be updated", func(t *testing.T) {})
 
 		t.Run("items cannot be added", func(t *testing.T) {})
