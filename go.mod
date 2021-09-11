a9a95cc324846bad1dabc2736eef112fe03ddb42
antklim <antklim@gmail.com>
2021-09-11 10:15:12 +1000

    Dynamo storage inited

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/antklim/go-invoice
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.40.41
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
 )
---
+++ storage/dynamo/dynamo.go
@@ -0,0 +1,35 @@
+package dynamo
+
+import (
+	"errors"
+
+	"github.com/antklim/go-invoice/invoice"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+type API interface {
+	PutItem(*dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
+	Query(*dynamodb.QueryInput) (*dynamodb.QueryOutput, error)
+}
+
+type Dynamo struct {
+	client API
+}
+
+func New(client API) *Dynamo {
+	return &Dynamo{client: client}
+}
+
+func (d *Dynamo) AddInvoice(inv invoice.Invoice) error {
+	return errors.New("not implemented")
+}
+
+func (d *Dynamo) FindInvoice(id string) (*invoice.Invoice, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (d *Dynamo) UpdateInvoice(inv invoice.Invoice) error {
+	return errors.New("not implemented")
+}
+
+var _ invoice.Storage = (*Dynamo)(nil)
