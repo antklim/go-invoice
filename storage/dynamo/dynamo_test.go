5e2bbb6d9babc379bf6ad9752d2a4f80dc3b89cb
antklim <antklim@gmail.com>
2021-09-14 09:34:24 +1000

    Testing PutItem input WIP

--- storage/dynamo/dynamo_test.go
+++ storage/dynamo/dynamo_test.go
@@ -11,6 +11,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+func testInvoiceItems(t *testing.T, dItems []dynamo.Item, items []invoice.Item) {
+	if len(dItems) != len(items) {
+		t.Errorf("invalid dInvoice.items %v, want %v", dItems, items)
+	}
+}
+
 func testPutItemInput(t *testing.T, inv invoice.Invoice, input *dynamodb.PutItemInput) {
 	if got, want := aws.StringValue(input.TableName), "invoices"; got != want {
 		t.Errorf("invalid PutItem input table %q, want %q", got, want)
@@ -28,6 +34,35 @@ func testPutItemInput(t *testing.T, inv invoice.Invoice, input *dynamodb.PutItem
 	if dinv.ID != inv.ID {
 		t.Errorf("invalid dInvoice.ID %q, want %q", dinv.ID, inv.ID)
 	}
+
+	if dinv.CustomerName != inv.CustomerName {
+		t.Errorf("invalid dInvoice.CustomerName %q, want %q", dinv.CustomerName, inv.CustomerName)
+	}
+
+	if (dinv.Date == nil && inv.Date != nil) ||
+		(dinv.Date != nil && inv.Date == nil) ||
+		(dinv.Date != nil && inv.Date != nil && !dinv.Date.Equal(*inv.Date)) {
+		t.Errorf("invalid dInvoice.Date %v, want %v", dinv.Date, inv.Date)
+	}
+
+	if dinv.Status != inv.Status.String() {
+		t.Errorf("invalid dInvoice.Status %q, want %q", dinv.Status, inv.Status.String())
+	}
+
+	testInvoiceItems(t, dinv.Items, inv.Items)
+
+	if !dinv.CreatedAt.Equal(inv.CreatedAt) {
+		t.Errorf("invalid dInvoice.CreatedAt %v, want %v", dinv.CreatedAt, inv.CreatedAt)
+	}
+
+	if !dinv.UpdatedAt.Equal(inv.UpdatedAt) {
+		t.Errorf("invalid dInvoice.UpdatedAt %v, want %v", dinv.UpdatedAt, inv.UpdatedAt)
+	}
+}
+
+func TestDynamoPKFormats(t *testing.T) {
+	t.Run("has valid invoice PK format", func(t *testing.T) {})
+	t.Run("has valid invoice item PK format", func(t *testing.T) {})
 }
 
 func TestAddInvoice(t *testing.T) {
