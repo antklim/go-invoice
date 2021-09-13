b462bfc916f0bba80ac783c45fac51fe25b15f22
antklim <antklim@gmail.com>
2021-09-13 21:15:51 +1000

    Preparing dynamodb mock

--- test/mocks/dynamo.go
+++ test/mocks/dynamo.go
@@ -1,21 +1,80 @@
 package mocks
 
 import (
-	"errors"
+	"sync"
 
 	"github.com/antklim/go-invoice/storage/dynamo"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+type dynamoOp int
+
+const (
+	putItem dynamoOp = iota
+	query
+)
+
+func dynamoOpFrom(op string) dynamoOp {
+	switch op {
+	case "PutItem":
+		return putItem
+	case "Query":
+		return query
+	default:
+		return -1
+	}
+}
+
 type DynamoAPI struct {
+	errors map[dynamoOp]error
+
+	sync.RWMutex // guards calls
+	callsTimes   map[dynamoOp]int
+	callsArgs    map[dynamoOp][]interface{}
+}
+
+func NewDynamoAPI() *DynamoAPI {
+	return &DynamoAPI{
+		callsTimes: make(map[dynamoOp]int),
+		callsArgs:  make(map[dynamoOp][]interface{}),
+	}
 }
 
 var _ dynamo.API = (*DynamoAPI)(nil)
 
 func (api *DynamoAPI) PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
-	return nil, errors.New("not implemented")
+	api.Lock()
+	defer api.Unlock()
+	api.recordPutItemCall(input)
+
+	return nil, api.errors[putItem]
 }
 
 func (api *DynamoAPI) Query(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
-	return nil, errors.New("not implemented")
+	api.Lock()
+	defer api.Unlock()
+	api.recordQueryCall(input)
+
+	return nil, api.errors[query]
+}
+
+func (api *DynamoAPI) CalledTimes(op string) int {
+	api.RLock()
+	defer api.RUnlock()
+
+	times, ok := api.callsTimes[dynamoOpFrom(op)]
+	if !ok {
+		return -1
+	}
+	return times
+}
+
+func (api *DynamoAPI) recordPutItemCall(input *dynamodb.PutItemInput) {
+	api.callsTimes[putItem]++
+	api.callsArgs[putItem] = append(api.callsArgs[putItem], input)
+}
+
+func (api *DynamoAPI) recordQueryCall(input *dynamodb.QueryInput) {
+	api.callsTimes[query]++
+	api.callsArgs[query] = append(api.callsArgs[query], input)
 }
