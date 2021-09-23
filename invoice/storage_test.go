09cb90047220d53c114c8560f604c1a75c877d08
antklim <antklim@gmail.com>
2021-09-23 15:19:44 +1000

    Storage tests improvements

--- invoice/storage_test.go
+++ invoice/storage_test.go
@@ -1,20 +1,26 @@
 package invoice_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/antklim/go-invoice/invoice"
 	"github.com/antklim/go-invoice/storage"
+	"github.com/google/uuid"
 )
 
-// TODO: switch between different storages based on env variable
+func storageSetup() invoice.Storage {
+	f := new(storage.Memory)
+	strg := f.MakeStorage()
+	return strg
+}
 
 func TestAddInvoiceFails(t *testing.T) {
 	t.Run("when repeat adding existing invoice", func(t *testing.T) {
-		f := new(storage.Memory)
-		strg := f.MakeStorage()
+		strg := storageSetup()
 
-		inv := invoice.NewInvoice("123", "customer")
+		invID := uuid.NewString()
+		inv := invoice.NewInvoice(invID, "John Doe")
 		if err := strg.AddInvoice(inv); err != nil {
 			t.Errorf("AddInvoice(%v) failed: %v", inv, err)
 		}
@@ -22,7 +28,7 @@ func TestAddInvoiceFails(t *testing.T) {
 		err := strg.AddInvoice(inv)
 		if err == nil {
 			t.Errorf("expected second call AddInvoice(%v) to fail", inv)
-		} else if got, want := err.Error(), `ID "123" exists`; got != want {
+		} else if got, want := err.Error(), fmt.Sprintf("ID %q exists", invID); got != want {
 			t.Errorf("second call AddInvoice(%v) = %v, want %v", inv, got, want)
 		}
 	})
@@ -30,10 +36,9 @@ func TestAddInvoiceFails(t *testing.T) {
 
 func TestFindInvoice(t *testing.T) {
 	t.Run("returns nil invoice when no invoices found", func(t *testing.T) {
-		f := new(storage.Memory)
-		strg := f.MakeStorage()
+		strg := storageSetup()
 
-		invID := "123"
+		invID := uuid.Nil.String()
 		inv, err := strg.FindInvoice(invID)
 		if err != nil {
 			t.Errorf("FindInvoice(%q) failed: %v", invID, err)
@@ -46,14 +51,15 @@ func TestFindInvoice(t *testing.T) {
 
 func TestUpdateInvoiceFails(t *testing.T) {
 	t.Run("when updating non-existing invoice", func(t *testing.T) {
-		f := new(storage.Memory)
-		strg := f.MakeStorage()
-		inv := invoice.NewInvoice("123", "customer")
+		strg := storageSetup()
+
+		invID := uuid.Nil.String()
+		inv := invoice.NewInvoice(invID, "John Doe")
 
 		err := strg.UpdateInvoice(inv)
 		if err == nil {
 			t.Errorf("expected UpdateInvoice(%v) to fail", inv)
-		} else if got, want := err.Error(), `invoice "123" not found`; got != want {
+		} else if got, want := err.Error(), fmt.Sprintf("invoice %q not found", invID); got != want {
 			t.Errorf("UpdateInvoice(%v) = %v, want %v", inv, got, want)
 		}
 	})
